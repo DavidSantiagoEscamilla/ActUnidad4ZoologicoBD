08fbce5e1f4e1acd0036450e2939ff1f845ef871
DavidSantiagoEscamilla <sr.davidsantiago@hotmail.com>
2025-07-08 17:16:04 -0500

    Cambios generados en clase Cuidador

--- src/main/java/com/davidsantiagoact2/clases/Cuidador.java
+++ src/main/java/com/davidsantiagoact2/clases/Cuidador.java
@@ -6,9 +6,9 @@ public class Cuidador {
     private int id_personal;
     
 
-    public Cuidador(int idCuidador, String nombre, String especialidad) {
-        this.idCuidador = idCuidador;
-        this.nombre = nombre;
+    public Cuidador(int idCuidador, int id_personal, String especialidad) {
+        this.id_cuidador = idCuidador;
+        this.id_personal = id_personal;
         this.especialidad = especialidad;
     }
 
