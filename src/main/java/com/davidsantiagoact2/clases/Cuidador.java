1b107d9339fb5008510b7e13cb7d58f814e8a355
DavidSantiagoEscamilla <144516482+DavidSantiagoEscamilla@users.noreply.github.com>
2025-07-08 17:07:04 -0500

    Actualizamos variables en cuidador

--- src/main/java/com/davidsantiagoact2/clases/Cuidador.java
+++ src/main/java/com/davidsantiagoact2/clases/Cuidador.java
@@ -1,22 +1,23 @@
 package com.davidsantiagoact2.clases;
 
 public class Cuidador {
-    private int idCuidador;
-    private String nombre;
+    private int id_cuidador;
     private String especialidad;
+    private int id_personal;
+    
 
     public Cuidador(int idCuidador, String nombre, String especialidad) {
-        this.idCuidador = idCuidador;
+        this.id_cuidador = id_cuidador;
         this.nombre = nombre;
         this.especialidad = especialidad;
     }
 
     public int getIdCuidador() {
-        return idCuidador;
+        return id_cuidador;
     }
 
-    public void setIdCuidador(int idCuidador) {
-        this.idCuidador = idCuidador;
+    public void setIdCuidador(int id_cuidador) {
+        this.id_cuidador = id_cuidador;
     }
 
     public String getNombre() {
